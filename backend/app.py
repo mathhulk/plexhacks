4bc4f55868524c8dcc05f643555be4d199e82a02
Daniel Gushchyan <d.gushchyan@gmail.com>
2020-08-09 03:08:16 -0700

    Final

--- backend/app.py
+++ backend/app.py
@@ -22,13 +22,18 @@ async def get_senate_list():
 
 @app.post("/bill/")
 async def get_bill():
-    bill = requests.get('')
+    bill = requests.get('https://api.propublica.org/congress/v1/bills/search.json')
     return bill.json()
 
 @app.post("/memberRequest/")
 async def get_memember(item: PoliticianID):
     member = requests.get('https://api.propublica.org/congress/v1/member/%s.json', item, headers=headers)    
     return member.json()
+
+@app.post("/votesRequest/")
+async def votesRequest(item: PoliticianID):
+    votes = requests.get('https://api.propublica.org/congress/v1/members/%s/votes.json', item, headers=headers)
+    return votes.json
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "127.0.0.1", port = 8000)
